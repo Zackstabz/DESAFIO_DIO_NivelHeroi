3b0bade22d6a54e145d163957df2e24cfb42654c
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-20 14:26:45 -0300

    Create Desafio_FOR.js

---
+++ Desafio_FOR.js
@@ -0,0 +1,32 @@
+// Nome e Experiência
+let nomeHeroi = "Felipão The Master 😎";
+let xpHeroi = 5000; // Valor inicial
+
+// Loop FOR simulando aumento de experiência
+for (let i = 0; i < 3; i++) { // Simulando 3 aumentos de experiência
+  xpHeroi += 2000; // Aumento de experiência 
+  
+  // Estrutura de Decisão para verificar o nível do herói
+  let nivelHeroi;
+
+  if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+  } else if (xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+  } else if (xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+  } else if (xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+  } else if (xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+  } else if (xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+  } else if (xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+  } else {
+    nivelHeroi = "Radiante";
+  }
+
+  // Mensagem com NOME e NÍVEL do herói e IMPLEMENTEI a Experiência!
+  console.log("O herói " + nomeHeroi + " está no nível " + nivelHeroi + " com " + xpHeroi + " de experiência.");
+}
